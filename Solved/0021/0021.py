4ddd90a74f8b0243739cbab2026cb79595528967
Jinmin Goh <eric970901@gmail.com>
2019-12-09 01:35:53 +0900

    LeetCode problem 21 solution

---
+++ Solved/0021/0021.py
@@ -0,0 +1,62 @@
+# Problem No.: 21
+# Solver:      Jinmin Goh
+# Date:        20191209
+# URL: https://leetcode.com/problems/merge-two-sorted-lists/submissions/
+
+import sys
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if not l1 and not l2:
+            return None
+        elif not l1:
+            return l2
+        elif not l2:
+            return l1
+        walker = None
+        head = None
+        temp1 = l1
+        temp2 = l2
+        while temp1 and temp2:
+            if head == None:
+                #print("before:",temp1.val, temp2.val)
+                if temp1.val < temp2.val:
+                    head = temp1
+                    walker = head
+                    #walker.next = None
+                    temp1 = temp1.next
+                else:
+                    head = temp2
+                    walker = head
+                    #walker.next = None
+                    temp2 = temp2.next
+                #print("after:",temp1.val, temp2.val)
+            else:
+                #print("before:",temp1.val, temp2.val)
+                if temp1.val < temp2.val:
+                    walker.next = temp1
+                    walker = walker.next
+                    temp1 = temp1.next
+                else:
+                    walker.next = temp2
+                    walker = walker.next
+                    temp2 = temp2.next
+                #print("after:",temp1.val, temp2.val)
+        if temp1:
+            walker.next = temp1
+        if temp2:
+            walker.next = temp2
+        return head
+        
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        

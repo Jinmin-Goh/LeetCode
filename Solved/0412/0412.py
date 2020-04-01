2a8cfd263b0d1b1311a809f0ae00ebdf0695033e
Jinmin Goh <eric970901@gmail.com>
2020-04-01 22:21:45 +0900

    LeetCode problem 412 solution

---
+++ Solved/0412/0412.py
@@ -0,0 +1,21 @@
+# Problem No.: 412
+# Solver:      Jinmin Goh
+# Date:        20200401
+# URL: https://leetcode.com/problems/fizz-buzz/
+
+import sys
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+                
+        return ans

9736cf23711e7b8cd941cda1f3c1414261bd92fe
Jinmin Goh <eric970901@gmail.com>
2020-06-06 00:41:48 +0900

    LeetCode problem 528 solution

---
+++ Solved/0528/0528.py
@@ -0,0 +1,13 @@
+# Problem No.: 528
+# Solver:      Jinmin Goh
+# Date:        20200606
+# URL: https://leetcode.com/problems/random-pick-with-weight/
+
+import sys
+import random
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
